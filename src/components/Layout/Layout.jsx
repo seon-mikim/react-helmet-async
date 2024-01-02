92f704931bb13ec96a6d58184ae527a75727b140
seon-mikim <tjsalsun@gmail.com>
2024-01-02 19:18:46 +0900

    feat: 헤더 푸터 레이아웃 사이드바 생성

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
